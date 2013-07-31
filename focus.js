f470e8f95b8f9c13ee0e8c551c015de31c06bafe
shylux <shylux@gmail.com>
2013-08-01 00:58:51 +0200

    added basic overlay with close button

--- focus.js
+++ focus.js
@@ -1,1 +1,17 @@
-alert("Hello World!");
+// overlay html
+var overlay = $('\
+  <div id="focusmanga_overlay">\
+    <div id="focusmanga_close">X</div>\
+    <img />\
+  </div>');
+
+
+$(function() {
+  // init
+  $('body').prepend(overlay);
+
+  // close overlay
+  $('#focusmanga_close').click(function() {
+    overlay.addClass('focusmanga_disabled');
+  });
+});
