8041d445d7c118e6b811f20b24dcc348bde9b88d
shylux <shylux@gmail.com>
2013-08-02 04:44:05 +0200

    added img to overlay. added resize logic. switched to dev jquery lib (min tried to load other sources)

--- focus.js
+++ focus.js
@@ -5,10 +5,25 @@ var overlay = $('\
     <img />\
   </div>');
 
-
+var w, h;
 $(function() {
   // init
   $('body').prepend(overlay);
+
+  $('img', overlay).load(function() {
+    w = this.width;
+    h = this.height;
+    $(window).resize();
+  });
+
+  $('img', overlay).attr('src', $('#mainImg').attr('src'));
+  $(window).resize(function() {
+    if ($(window).width() < w) {
+      $('img', overlay).addClass('landscape');
+    } else {
+      $('img', overlay).removeClass('landscape');
+    }
+  });
 
   // close overlay
   $('#focusmanga_close').click(function() {
