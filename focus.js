852c723d07683705acb55ab6184c81662e7bade1
shylux <shylux@gmail.com>
2013-08-02 18:29:24 +0200

    added keyboard listener. added page preload.

--- focus.js
+++ focus.js
@@ -7,7 +7,7 @@ var overlay = $('\
 $('#fm_close', overlay).attr('src', chrome.extension.getURL('img/close-circle.png'));
 
 var w, h;
-$(function() {
+$('body').ready(function() {
   // init
   $('body').prepend(overlay);
 
@@ -33,4 +33,24 @@ $(function() {
   $('#fm_close').click(function() {
     overlay.addClass('fm_disabled');
   });
+
+  // keyboard bindings
+  $(document).keydown(function(e) {
+    switch (e.keyCode) {
+      case 39: // arrow right
+        break;
+      case 37: // arrow left
+        break;
+      case 27: // escape
+        overlay.addClass('fm_disabled');
+        break;
+      default:
+        console.log('pressed kay: '+e.keyCode);
+    }
+  });
+
+  // preload next page (doesn't work atm. Chrome 30.0.1581.2 dev-m)
+  $('head').append(
+      $('<link rel="prerender" />').attr('src', $('.next').parent().attr('href'))
+  );
 });
