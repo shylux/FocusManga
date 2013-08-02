0e8e92b41246ddfafcbc844df979413220f4df9b
shylux <shylux@gmail.com>
2013-08-02 20:22:45 +0200

    added timer.

--- focus.js
+++ focus.js
@@ -3,10 +3,14 @@ var overlay = $('\
   <div id="fm_overlay">\
     <img id="fm_close" />\
     <img id="fm_main" />\
+    <div id="fm_tools">\
+      <img id="fm_play">\
+    </div>\
   </div>');
 $('#fm_close', overlay).attr('src', chrome.extension.getURL('img/close-circle.png'));
+$('#fm_play', overlay).attr('src', chrome.extension.getURL('img/play.png'));
 
-var w, h;
+var w, h, timer;
 $('body').ready(function() {
   // init
   $('body').prepend(overlay);
@@ -49,8 +53,36 @@ $('body').ready(function() {
     }
   });
 
+  // timer
+  if (typeof(localStorage['timer_state']) == 'undefined') localStorage['timer_state'] = false;
+  timer = $.timer(function() {
+    $('.next').click();
+  }, 3000, (localStorage['timer_state']=='true'));
+
+  //timer.set({time: 3000, autostart: start});
+  updateTimerIcon();
+  $('#fm_play').click(function() {
+    if (timer.isActive) {
+      localStorage['timer_state'] = false;
+      timer.stop();
+    } else {
+      localStorage['timer_state'] = true;
+      timer.once(0);
+    }
+    updateTimerIcon();
+  });
+  
+
   // preload next page (doesn't work atm. Chrome 30.0.1581.2 dev-m)
   $('head').append(
       $('<link rel="prerender" />').attr('src', $('.next').parent().attr('href'))
   );
 });
+
+function updateTimerIcon() {
+  if ((localStorage['timer_state']=='true')) {
+    $('#fm_play', overlay).attr('src', chrome.extension.getURL('img/stop.png'));
+  } else {
+    $('#fm_play', overlay).attr('src', chrome.extension.getURL('img/play.png'));
+  }
+}
