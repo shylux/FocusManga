b41eed09ac3621fea981e42d264d76bbb3475f2d
Lukas Knöpfel <shylux@gmail.com>
2013-08-24 18:07:32 +0200

    now open options page on install. closes #6.

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,13 @@
+// open options page on install
+function install_notice() {
+    if (localStorage.getItem('install_time'))
+        return;
+
+    var now = new Date().getTime();
+    localStorage.setItem('install_time', now);
+    chrome.tabs.create({url: "options.html"});
+}
+install_notice();
 
 // default value
 if (!localStorage['focusmanga_enabled']) localStorage['focusmanga_enabled'] = true;
